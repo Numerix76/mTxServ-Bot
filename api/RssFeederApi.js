84e558758d25ba86cfda81a6293f0a80e9977198
Seb <sdieunidou@users.noreply.github.com>
2022-03-09 11:49:49 +0100

    Update RssFeederApi.js

--- api/RssFeederApi.js
+++ api/RssFeederApi.js
@@ -3,6 +3,7 @@ const Parser = require('rss-parser')
 class RssFeederApi {
 	async get(feedUrl) {
 		let parser = new Parser();
+		console.log('try to parse RSS feed: ' + feedUrl);
 		return await parser.parseURL(feedUrl);
 	}
 }
