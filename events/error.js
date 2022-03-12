7e0975d04e6a9199b204684ac52eaa84755fa1df
BenjaminFreval <benjamin.freval@etu.univ-lehavre.fr>
2022-03-12 18:08:16 +0100

    Added more field for errors

--- events/error.js
+++ events/error.js
@@ -13,6 +13,21 @@ module.exports = {
 			.setDescription(error.stack ? `\`\`\`x86asm\n${error.stack.substr(0, 2048)}\n\`\`\`` : `\`${error.toString().substr(0, 2048)}\``)
 			.setTimestamp();
 
+		if ( error.method )
+			embed.addField("Method", error.method, true)
+
+		if ( error.path )
+			embed.addField("Path", error.path, true)
+
+		if ( error.code )
+			embed.addField("Code", `${error.code}`, true)
+
+		if ( error.httpStatus )
+			embed.addField("httpStatus", error.httpStatus, true)
+
+		if ( error.requestData )
+			embed.addField("requestData", `${error.requestData}`, true)
+
 		mTxServUtil.sendLogMessage(embed)
 	}
 };
