82cef107b43add6daecfc452d118cfdc8170ad13
BenjaminFreval <benjamin.freval@etu.univ-lehavre.fr>
2022-03-17 13:27:23 +0100

    debug

--- services/StatusMonitor.js
+++ services/StatusMonitor.js
@@ -19,16 +19,16 @@ module.exports = class FeedMonitor {
 		if (embed.fields.length === 0)
 			embed.setDescription("No servers have problems")
 
-		for (const guild of client.guilds.cache)
+		for (const guild of client.guilds.cache.map(guild => guild))
 		{
 			//let statusMessage
 		
-			const currentConfig = await client.provider.get('status', guild[0], "")
-			const statusChannel = await guild[1].channels.cache.get(currentConfig.channel)
+			const currentConfig = await client.provider.get('status', guild.id, "")
+			const statusChannel = await guild.channels.cache.get(currentConfig.channel)
 			const statusMessage = await statusChannel?.messages.cache.get(currentConfig.message)
 			//await statusChannel?.messages.fetch(currentConfig.message).then(message => statusMessage = message).catch(console.error)
 
-			if ( guild[0] === "894658407330639875" || guild[0] === "529605510219956233" )
+			if ( guild.id === "894658407330639875" || guild.id === "529605510219956233" )
 				mTxServUtil.sendLogMessage( mTxServUtil.sayError(null, `url = ${this.url} channel = ${currentConfig.channel} message = ${currentConfig.message}`) )
 
 			if (!statusMessage)
