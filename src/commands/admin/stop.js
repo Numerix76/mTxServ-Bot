94fbb3805198f2f79ecb7b4cc5b81998e4bb22e9
Numerix76 <mbvcf@free.fr>
2022-06-27 21:35:16 +0200

    Readd stop-bot

---
+++ src/commands/admin/stop.js
@@ -0,0 +1,42 @@
+const { Command, Constants } = require("sheweny");
+const mTxServUtil = require("../../util/mTxServUtil");
+const { EmbedBuilder, Colors } = require("discord.js");
+
+module.exports = class LangSelectorCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "stop-bot",
+			nameLocalizations: {
+				'fr': 'stop-bot'
+			},
+			description: "Shutdown the bot.",
+			descriptionLocalizations: {
+				'fr': 'Arrête le bot.',
+			},
+			category: "Admin",
+			userPermissions: ["SendMessages"],
+			channel: Constants.COMMAND_CHANNEL.guild,
+			adminsOnly: true
+		});
+	}
+
+	async execute(interaction) {	
+		const embed = new EmbedBuilder()
+			.setAuthor({ name: client.user.tag, iconURL: client.user.displayAvatarURL() })
+			.setColor(Colors.Red)
+			.setTitle(':red_circle: Bot is offline')
+			.setTimestamp();
+
+		await mTxServUtil.sendLogMessage({
+			embeds: [embed]
+		});
+
+		const response = mTxServUtil.sayError(interaction, mTxServUtil.translate(interaction, ["stop-bot","success"]))
+
+		await interaction.reply({
+			embeds: [response]
+		});
+		
+		process.exit();
+	}
+}
