17669743fe0b101af2ab5c87560111bbd3656fa6
Numerix76 <mbvcf@free.fr>
2022-06-29 18:06:10 +0200

    Readd mtx and server operator role

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,7 +1,6 @@
+const { ChannelType } = require("discord.js");
 const { Event } = require("sheweny");
-const mTxServUtil = require("../util/mTxServUtil");
-const { EmbedBuilder, Colors, ChannelType } = require("discord.js");
-const { Constants } = require("sheweny");
+const mTxServApi = require("../api/mTxServApi.js");
 
 module.exports = class messageCreateEvent extends Event {
 	constructor(client) {
@@ -12,7 +11,9 @@ module.exports = class messageCreateEvent extends Event {
 	}
 
 	async execute(msg) {
-		if (msg.channel.type !== ChannelType.GuildText) return;
+		const authorizeChannel = [ChannelType.GuildForum, ChannelType.GuildPrivateThread, ChannelType.GuildPublicThread, ChannelType.GuildText];
+
+		if (authorizeChannel.indexOf(msg.channel.type) === -1) return;
 		if (msg.author.bot) return;
 		if ( isDev ) return;
 		if ( !client.isMainGuild(msg.guild.id) ) return;
@@ -22,6 +23,31 @@ module.exports = class messageCreateEvent extends Event {
 		/*------------------*/
 		if ( msg.member.displayName ) {
 			client.ranker.processMessage(msg);
+		}
+
+		/*--------------------*/
+		/* mTxServ user role  */
+		/*--------------------*/
+		const mTxServUserApi = new mTxServApi()
+		const roleMtxServ = '773540951434985503';
+		if ( await mTxServUserApi.isAuthenticated(msg.author.id) ) 
+		{
+			const role = await msg.guild.roles.fetch(roleMtxServ)
+			if (role && !msg.member.roles.cache.has(role.id)) {
+				msg.member.roles.add(role).catch(console.error);
+			}
+		}
+
+		/*----------------------*/
+		/* GameServer user role */
+		/*----------------------*/
+		const roleGameServ = '773500803218538546'
+		if ( -1 !== msg.channel.name.indexOf('serveurs') || -1 !== msg.channel.name.indexOf('servers') )
+		{
+			const role = await msg.guild.roles.fetch(roleGameServ)
+			if ( role && !msg.member.roles.cache.has(role.id) ) {
+				msg.member.roles.add(role).catch(console.error);
+			}
 		}
 	}
 };
