0547cfa6125af8560a695a5a911274ddf1937f34
Seb <seb@hostme.fr>
2022-07-21 15:40:27 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+      '@typescript-eslint/no-var-requires': 0,
+      'no-undef': 0,
+    }
+}
